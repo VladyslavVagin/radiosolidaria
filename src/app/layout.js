052cfedbaa52db6429ecbfa6068a9cc4106e38ef
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-06-15 10:05:41 +0200

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,97 @@
 "use client";
 
 import { Suspense } from "react";
-import "./globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
-
-const metadata = {
-  title: "Solidaria Media",
-  description:
-    "Solidaria Media es una agencia de comunicación especializada en proyectos de impacto social y medioambiental. Ayudamos a organizaciones y empresas a contar sus historias de manera efectiva y a conectar con sus audiencias de forma auténtica y sostenible.",
-};
-
+import "./globals.css";
 
 export default function RootLayout({ children }) {
-
   return (
-    <html lang="es">
+    <html lang="es-ES">
+      <head>
+        <title>Solidaria RTV - radio y televisión</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/favicon/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/favicon/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/favicon/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/favicon/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/favicon/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/favicon/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/favicon/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/favicon/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/favicon/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/manifest.json" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/favicon/ms-icon-144x144.png" />
+        <meta name="theme-color" content="#ffffff"></meta>
+      </head>
       <body>
-          <Header />
-          <main>
-            <Suspense fallback={<p>Loading...</p>}>{children}</Suspense>
-          </main>
-          <Footer />
+        <Header />
+        <main>
+          <Suspense fallback={<p>Loading...</p>}>{children}</Suspense>
+        </main>
+        <Footer />
       </body>
     </html>
   );
