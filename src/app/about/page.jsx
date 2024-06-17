1c04ea8ab1f39acf9dde5a4f6a1688fc309073f1
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-06-17 18:51:48 +0200

    update socials

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import ListSocials from "@/components/aboutPage/ListSocials/ListSocials";
+import css from "./page.module.css";
 
 const page = () => {
   return (
-    <div>
-      Sobre Nosotros
-    </div>
-  )
-}
+    <section>
+      <div className={css.container}>
+        <h1>Medios de Comunicación Bíblicas</h1>
+        <ListSocials />
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default page;
