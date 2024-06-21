d45a3b4c88f6457d3f144f832847b722d0f9d778
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-06-21 16:18:11 +0200

    favicon y metadata

--- src/app/tv/page.jsx
+++ src/app/tv/page.jsx
@@ -8,6 +8,18 @@ export const metadata = {
   title: "Solidaria TV - Televisión cristiana en línea",
   description:
     "Solidaria TV es una televisión cristian en línea que transmite programas y mensajes cristianos las 24 horas del día.",
+  icons: {
+    icon: "/faviconTV/favicon.ico",
+    appleIcon: "/faviconTV/apple-touch-icon.png",
+    msIcon: "/faviconTV/mstile-150x150.png",
+    favicon16: "/faviconTV/favicon-16x16.png",
+    favicon32: "/faviconTV/favicon-32x32.png",
+    androidIcon: "/faviconTV/android-chrome-192x192.png",
+    androidIcon512: "/faviconTV/android-chrome-512x512.png",
+    safariPinnedTab: "/faviconTV/safari-pinned-tab.svg",
+    browseConfig: "/faviconTV/browserconfig.xml",
+    siteManifest: "/faviconTV/site.webmanifest",
+  },
 };
 
 const page = () => {
