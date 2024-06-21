8580df672034b124b576abcb4115a7ab935b776e
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-06-21 16:05:15 +0200

    favicon radio

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -7,6 +7,18 @@ export const metadata = {
   title: "Solidaria RTV - Nuestras afiliaciones, sitios web y redes sociales",
   description:
     "Solidaria RTV es una radio y televisión cristiana en línea que transmite música, programas y mensajes cristianos las 24 horas del día.",
+  icons: {
+    icon: "/favicon.ico",
+    appleIcon: "/apple-touch-icon.png",
+    msIcon: "/mstile-150x150.png",
+    favicon16: "/favicon-16x16.png",
+    favicon32: "/favicon-32x32.png",
+    androidIcon: "/android-chrome-192x192.png",
+    androidIcon512: "/android-chrome-512x512.png",
+    safariPinnedTab: "/safari-pinned-tab.svg",
+    browseConfig: "/browserconfig.xml",
+    siteManifest: "/site.webmanifest",
+  },
 };
 
 const page = () => {
